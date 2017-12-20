4c99907265da7e9824aeadc424cec3fb867cf65f
Magda Balun <magi@op.pl>
2017-12-20 13:39:35 +0100

    Change .js file and format files

--- scripts.js
+++ scripts.js
@@ -12,9 +12,20 @@ function Dimensions(type, length, width, thickness, weight) {
     this.weight = weight;
 }
 
+function Discount(type, price, discount) {
+    this.type = type;
+    this.price = price;
+    this.discount = discount;
+    this.price = price - discount;
+}
+
 Phone.prototype.printInfo = function() {
     console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
-};
+}
+
+Discount.prototype.applyDiscount = function() {
+    console.log('The ' + this.type + ' new price is ' + this.price + '.');
+}
 
 Dimensions.prototype.showDimensions = function() {
     console.log('The ' + this.type + ' length is ' + this.length + ' mm, width is ' + this.width + ' mm, the thickness is ' + this.thickness + ' mm and the weight is ' + this.weight + ' g.');
@@ -27,6 +38,14 @@ var onetPlusOne = new Phone('Onet', 999, 'black');
 samsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
 onetPlusOne.printInfo();
+
+samsungGalaxyS6 = new Discount('Samsung Galaxy S6', 1500, 100);
+iPhone6S = new Discount('iPhone 6S', 1800, 150);
+onetPlusOne = new Discount('OnetPlusOne', 999, 200);
+
+samsungGalaxyS6.applyDiscount();
+iPhone6S.applyDiscount();
+onetPlusOne.applyDiscount();
 
 samsungGalaxyS6 = new Dimensions('Samsung Galaxy S6', 1500, 600, 8, 150);
 iPhone6S = new Dimensions('iPhone 6S', 1900, 900, 10, 200);
