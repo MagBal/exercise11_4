fccbc14ccf08e878f5544b9b361b945e8b764de9
Magda Balun <magi@op.pl>
2017-12-21 22:57:03 +0100

    Change .js file

--- scripts.js
+++ scripts.js
@@ -1,56 +1,48 @@
-function Phone(brand, price, color) {
-    this.brand = brand;
+function Phone(/*brand, */price, /* color, */discount) {
+    //this.brand = brand;
     this.price = price;
-    this.color = color;
+    //this.color = color;
+    this.discount = discount;
+
 }
 
-function Dimensions(type, length, width, thickness, weight) {
+/*function Dimensions(type, length, width, thickness, weight) {
     this.type = type;
     this.length = length;
     this.width = width;
     this.thickness = thickness;
     this.weight = weight;
-}
-
-function Discount(type, price, discount) {
-    this.type = type;
-    this.price = price;
-    this.discount = discount;
-    this.price = price - discount;
-}
+}*/
 
 Phone.prototype.printInfo = function() {
-    console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
+    console.log(this.price);  
 }
 
-Discount.prototype.applyDiscount = function() {
-    console.log('The ' + this.type + ' new price is ' + this.price + '.');
+Phone.prototype.applyDiscount = function(discount) {
+    this.discount = discount;
+    console.log(this.price-this.discount);
 }
 
-Dimensions.prototype.showDimensions = function() {
+/*Dimensions.prototype.showDimensions = function() {
     console.log('The ' + this.type + ' length is ' + this.length + ' mm, width is ' + this.width + ' mm, the thickness is ' + this.thickness + ' mm and the weight is ' + this.weight + ' g.');
-}
+}*/
 
-var samsungGalaxyS6 = new Phone('Samsung', 1500, 'white');
-var iPhone6S = new Phone('Apple', 1800, 'white');
-var onetPlusOne = new Phone('Onet', 999, 'black');
+var samsungGalaxyS6 = new Phone(1500);
+var iPhone6S = new Phone(1800);
+var onetPlusOne = new Phone(999);
 
 samsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
 onetPlusOne.printInfo();
 
-samsungGalaxyS6 = new Discount('Samsung Galaxy S6', 1500, 100);
-iPhone6S = new Discount('iPhone 6S', 1800, 150);
-onetPlusOne = new Discount('OnetPlusOne', 999, 200);
+samsungGalaxyS6.applyDiscount(100);
+iPhone6S.applyDiscount(150);
+onetPlusOne.applyDiscount(99);
 
-samsungGalaxyS6.applyDiscount();
-iPhone6S.applyDiscount();
-onetPlusOne.applyDiscount();
-
-samsungGalaxyS6 = new Dimensions('Samsung Galaxy S6', 1500, 600, 8, 150);
+/*samsungGalaxyS6 = new Dimensions('Samsung Galaxy S6', 1500, 600, 8, 150);
 iPhone6S = new Dimensions('iPhone 6S', 1900, 900, 10, 200);
 onetPlusOne = new Dimensions('OnetPlusOne', 1200, 400, 10, 150);
 
 samsungGalaxyS6.showDimensions();
 iPhone6S.showDimensions();
-onetPlusOne.showDimensions();
+onetPlusOne.showDimensions();*/
